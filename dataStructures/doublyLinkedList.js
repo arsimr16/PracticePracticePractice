301b14111f71365ddb9393d47902ae1d35c5e318
arsimr16 <arsimr16@g.holycross.edu>
2017-11-29 18:13:16 -0800

    create file for doublyLinkedList

---
+++ dataStructures/doublyLinkedList.js
@@ -0,0 +1,6 @@
+// implement a doubly linked list with addToTail, removeHead, contains, and makeNode methods
+
+// more methods: addToHead, insertAfter, insertBefore, remove, forEach, map, length, kth item from head, kth item from tail
+
+// write time and space complexities for all methods
+// test all methods
