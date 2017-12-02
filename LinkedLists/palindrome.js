0975dd60597bc37674e994d7535fd347b9eabd53
arsimr16 <arsimr16@g.holycross.edu>
2017-12-02 13:52:03 -0800

    write tests for isPalindrome

--- LinkedLists/palindrome.js
+++ LinkedLists/palindrome.js
@@ -23,7 +23,7 @@ class LinkedList {
 }
 
 // I: a linked list
-// O: a boolead
+// O: a boolean
 // C: none
 // E: even and odd numbered lists, empty lists, case-sensitive
 const isPalindrome = (list) => {
@@ -40,3 +40,39 @@ const assertEquals = (actual, expected, testname) => {
 		console.log(`FAILED ${testname}: expected "${expected}", but got "${actual}"`);
 	}
 };
+
+let oddPalindrome = new LinkedList();
+oddPalindrome.addToTail('a');
+oddPalindrome.addToTail('b');
+oddPalindrome.addToTail('c');
+oddPalindrome.addToTail('b');
+oddPalindrome.addToTail('a');
+
+assertEquals(isPalindrome(oddPalindrome), true, 'should return true for odd length palindrome');
+
+let evenPalindrome = new LinkedList();
+evenPalindrome.addToTail('a');
+evenPalindrome.addToTail('b');
+evenPalindrome.addToTail('b');
+evenPalindrome.addToTail('a');
+
+assertEquals(isPalindrome(evenPalindrome), true, 'should return true for even length palindrome');
+
+let notPalindrome = new LinkedList();
+notPalindrome.addToTail('a');
+notPalindrome.addToTail('b');
+notPalindrome.addToTail('c');
+
+assertEquals(isPalindrome(notPalindrome), false, 'should return false for non-palindromes');
+
+let caseSensitive = new LinkedList();
+caseSensitive.addToTail('A');
+caseSensitive.addToTail('B');
+caseSensitive.addToTail('b');
+caseSensitive.addToTail('a');
+
+assertEquals(isPalindrome(caseSensitive), false, 'should be case-sensitive');
+
+let emptyList = new LinkedList();
+
+assertEquals(isPalindrome(emptyList), true, 'should return true when passed an empty list');
