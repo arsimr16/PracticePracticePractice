871c15b6a9bef6961c3045c69589f484643664fd
arsimr16 <arsimr16@g.holycross.edu>
2017-12-02 14:10:30 -0800

    pass all tests for isPalindrome()

--- LinkedLists/palindrome.js
+++ LinkedLists/palindrome.js
@@ -20,17 +20,37 @@ class LinkedList {
 		}
 		this.tail = newTail;
 	}
+
+	forEach(cb) {
+		let curr = this.head;
+		while(curr) {
+			cb(curr.value);
+			curr = curr.next;
+		}
+	}
 }
 
 // I: a linked list
 // O: a boolean
 // C: none
-// E: even and odd numbered lists, empty lists, case-sensitive
+// E: even and odd numbered lists, empty lists, case-sensitive, 
+// assume values are strings or numbers, not booleans, objects, arrays, functions, etc.
 const isPalindrome = (list) => {
-
+	let values = [];
+	list.forEach(item => values.push(item));
+	return values.join('') === values.reverse().join('');
+	// an alternate approach without reverse() is more efficient, but I like how easy reverse() is to read and implement:
+	// while (i < Math.floor(values.length / 2)) {
+  //   if (values[i] === values[values.length - 1 - i]) {
+  //	   i += 1;
+  //   } else {
+  //     return false;
+  //   }
+  //   return true;
+	// }
 };
-// time complexity:
-// space complexity:
+// time complexity: O(n) ? maybe O(n^2), I'm not sure about the time complexity of reverse()
+// space complexity: O(n)
 
 // tests
 const assertEquals = (actual, expected, testname) => {
