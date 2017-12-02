6a5125cd8c523a373c36c218ac155b3016dc3e0c
arsimr16 <arsimr16@g.holycross.edu>
2017-12-02 12:00:42 -0800

    implement sumListsReverse, write and pass tests for sumListsReverse

--- LinkedLists/sumLists.js
+++ LinkedLists/sumLists.js
@@ -29,29 +29,102 @@ class LinkedList {
 		this.tail = newTail;
 	}
 
-
-	// I: two linked lists, digits in reverse order
-	// O: a linked list containing sum digits in reverse order
-	// C: none
-	// E: empty list, numbers not same length, assume only positive integers
-	sumListsReverse(l1, l2) {
-		// sum lists with digits in reverse order
-	}
-	// time complexity:
-	// space complexity:
-
-
-	// I: two linked lists, digits in forward order
-	// O: a linked lists containing sum digits in forward order
-	// C: none
-	// E: empty list, numbers have different lengths, assume only positive integers
-	sumLists(l1, l2) {
-		// sum lists with digits in forward order
+	forEach(cb) {
+		let curr = this.head;
+		while(curr) {
+			// call the callback function on each item in the LinkedList
+			cb(curr.value);
+			curr = curr.next;
+		}
 	}
 }
 
-// assertion function(s)
+
+// I: two linked lists, digits in reverse order
+// O: a linked list containing sum digits in reverse order
+// C: none
+// E: empty list, numbers not same length, assume only positive integers
+const sumListsReverse = (l1, l2) => {
+	let result = new LinkedList;
+	let curr1 = l1.head;
+	let curr2 = l2.head;
+	let remainder = 0;
+	while (curr1 || curr2 || remainder) {
+		let val1 = curr1 ? curr1.value : 0;
+		let val2 = curr2 ? curr2.value : 0;
+		let sum = val1 + val2 + remainder;
+		if (sum >= 10) {
+			sum -= 10;
+			remainder = 1;
+		} else {
+			remainder = 0;
+		}
+		result.addToTail(sum);
+		curr1 = curr1 ? curr1.next : 0;
+		curr2 = curr2 ? curr2.next : 0;
+	}
+	return result;
+}
+// time complexity:
+// space complexity:
+
+
+// I: two linked lists, digits in forward order
+// O: a linked lists containing sum digits in forward order
+// C: none
+// E: empty list, numbers have different lengths, assume only positive integers
+const sumLists = (l1, l2) => {
+	// sum lists with digits in forward order
+}
+// time complexity:
+// space complexity:
+
+
+const assertDeepEquals = (actual, expected, testname) => {
+	if (JSON.stringify(actual) === JSON.stringify(expected)) {
+		console.log(`passed ${testname}`);
+	} else {
+		console.log(`FAILED ${testname}: expected "${expected}", but got "${actual}"`);
+	}
+};
 
 // tests for sumListsReverse()
+let list1 = new LinkedList();
+list1.addToTail(7);
+list1.addToTail(1);
+list1.addToTail(6);
+
+let list2 = new LinkedList();
+list2.addToTail(5);
+list2.addToTail(9);
+list2.addToTail(2);
+
+let list3 = new LinkedList();
+list3.addToTail(5);
+list3.addToTail(9);
+list3.addToTail(2);
+list3.addToTail(8);
+
+let list4 = new LinkedList();
+list4.addToTail(9);
+list4.addToTail(9);
+
+const reverseSum0 = sumListsReverse(list1, list2);
+const result0 = [];
+reverseSum0.forEach(item => result0.push(item));
+
+assertDeepEquals(result0, [2, 1, 9], 'should return sum of digits in reverse order');
+
+const reverseSum1 = sumListsReverse(list1, list3);
+const result1 = [];
+reverseSum1.forEach(item => result1.push(item));
+
+assertDeepEquals(result1, [2, 1, 9, 8], 'should return correct sum when lists are unequal in length');
+
+const reverseSum2 = sumListsReverse(list4, list4);
+const result2 = [];
+reverseSum2.forEach(item => result2.push(item));
+
+assertDeepEquals(result2, [8, 9, 1], 'should carry remainder of highest place value');
 
 // tests for sumLists()
