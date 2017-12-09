6a8e96b6eb450b89ba9971928b63ec8e1fbeda79
arsimr16 <arsimr16@g.holycross.edu>
2017-12-09 13:15:48 -0800

    write out problem

---
+++ stacksAndQueues/setOfStacks.js
@@ -0,0 +1,11 @@
+// imagine a literal stack of plates. If the stack gets too high, it might topple.
+// Therefore in real life we would likely start a new stack when the previous stack
+// exceeds some threshold.  
+
+// implement a data structure SetOfStacks that mimics this.  It should be composed of
+// several stacks and should create a new stack when the previous one exceeds capacity
+// push and pop should behave identically to the methods of for a single stack
+
+// FOLLOW UP:
+// implement a popAt(index) method which performs a pop operation on a specific sub-stack
+
