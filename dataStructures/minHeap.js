f9aaed541f52f30d89e07cc30d4090e0f58faf07
arsimr16 <arsimr16@g.holycross.edu>
2017-12-11 14:52:10 -0800

    fix bug in removeRoot (placement of return statement)

--- dataStructures/minHeap.js
+++ dataStructures/minHeap.js
@@ -1,10 +1,10 @@
 // min heap with insert and removeRoot methods
 
-class binaryHeap {
-	constructor() {
+class BinaryHeap {
+	constructor(compare) {
 		this.heap = [];
-		// currently compare function for a minHeap
-		this.compare = (a, b) => a < b;
+		// compare fn by which the heap is ordered (e.g. minHeap, maxHeap)
+		this.compare = compare;
 	}
 
 	swap(index, parentIndex) {
@@ -48,14 +48,48 @@ class binaryHeap {
 		// compare children to get index of appropriate child
 		let childIndex = this.getChildIndex(tempRootIndex);
 		// while there are children and the compare fn is true
-		while(childIndex && this.compare(this.heap[childIndex], this.heap[tempRootIndex]))
-			// swap child with root
-			this.swap(childIndex, tempRootIndex);
-			tempRootIndex = childIndex;
-			childIndex = this.getChildIndex(tempRootIndex);
+		while(childIndex && this.compare(this.heap[childIndex], this.heap[tempRootIndex])) {
+				// swap child with root
+				this.swap(childIndex, tempRootIndex);
+				tempRootIndex = childIndex;
+				childIndex = this.getChildIndex(tempRootIndex);
+		}
 		// return removed node from earlier
 		return origRoot;
 	}
 }
 
 // tests
+const assertDeepEquals = (actual, expected, testname) => {
+	if (JSON.stringify(actual) === JSON.stringify(expected)) {
+		console.log(`passed ${testname}`);
+	} else {
+		console.log(`FAILED ${testname}: expected "${expected}", but got "${actual}"`);
+	}
+}
+
+const testHeap = new BinaryHeap((a, b) => a < b);
+testHeap.insert(50);
+testHeap.insert(90);
+testHeap.insert(7);
+testHeap.insert(87);
+testHeap.insert(4);
+testHeap.insert(55);
+
+assertDeepEquals(testHeap.heap, [4, 7, 50, 90, 87, 55], 'should correctly insert values according to compare function');
+
+testHeap.removeRoot();
+assertDeepEquals(testHeap.heap, [7, 55, 50, 90, 87], 'should correctly remove root of heap');
+
+const testMaxHeap = new BinaryHeap((a, b) => a > b);
+testMaxHeap.insert(50);
+testMaxHeap.insert(90);
+testMaxHeap.insert(7);
+testMaxHeap.insert(87);
+testMaxHeap.insert(4);
+testMaxHeap.insert(55);
+
+assertDeepEquals(testMaxHeap.heap, [90, 87, 55, 50, 4, 7], 'should accept different compare functions');
+
+testMaxHeap.removeRoot();
+assertDeepEquals(testMaxHeap.heap, [87, 50, 55, 7, 4], 'should correctly remove root of heap with diff compare fn');
