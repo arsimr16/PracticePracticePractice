70b4382f7e15651b54204288e03f886d52301aab
arsimr16 <arsimr16@g.holycross.edu>
2017-12-12 09:09:50 -0800

    implement graph using adjacency matrix

--- dataStructures/graph.js
+++ dataStructures/graph.js
@@ -14,7 +14,7 @@ class Graph {
 
 	removeNode(value) {
 		if (this.contains(value)) {
-			for (targetNode in this.nodes[value].edges) {
+			for (let targetNode in this.nodes[value].edges) {
 				this.removeEdge(value, targetNode);
 			}
 			delete this.nodes[value];
@@ -29,19 +29,17 @@ class Graph {
 	}
 
 	addEdge(fromNode, toNode) {
-		if (!this.contains(fromNode) || !this.contains(toNode)) {
-			return;
+		if (this.contains(fromNode) && this.contains(toNode)) {
+			this.nodes[fromNode].edges[toNode] = toNode;
+			this.nodes[toNode].edges[fromNode] = fromNode;
 		}
-		this.nodes[fromNode].edges[toNode] = toNode;
-		this.nodes[toNode].edges[fromNode] = fromNode;
 	}
 
 	removeEdge(fromNode, toNode) {
-		if (!this.contains(fromNode) || this.contains(toNode)) {
-			return;
+		if (this.contains(fromNode) && this.contains(toNode)) {
+			delete this.nodes[fromNode].edges[toNode];
+			delete this.nodes[toNode].edges[fromNode];
 		}
-		delete this.nodes[fromNode].edges[toNode];
-		delete this.nodes[toNode].edges[fromNode];
 	}
 
 	forEachNode(cb) {
@@ -61,5 +59,51 @@ class Graph {
 
 // graph implementation using adjacency matrix
 class GraphAM {
+	constructor() {
+		this.nodes = [];
+	}
 
+	addNode(value)  {
+		this.nodes[value] = this.nodes[value] || [];
+	}
+
+	contains(value) {
+		return !!this.nodes[value];
+	}
+
+	removeNode(value) {
+		if (this.contains(value)) {
+			for (let i in this.nodes[value]) {
+				this.removeEdge(node, i);
+			}
+			this.nodes[value] = undefined;
+		}
+	}
+
+	addEdge(fromNode, toNode) {
+		if (this.contains(fromNode) && this.contians(toNode)) {
+			this.nodes[fromNode][toNode] = true;
+			this.nodes[toNode][fromNode] = true;
+		}
+	}
+
+	removeEdge(fromNode, toNode) {
+		if (this.contains(fromNode) && this.contains(toNode)) {
+			this.nodes[fromNode][toNode] = undefined;
+			this.nodes[toNode][fromNode] = undefined;
+		}
+	}
+
+	hasEdge(fromNode, toNode) {
+		if (!this.contains(fromNode)) {
+			return false;
+		}
+		return !!this.nodes[fromNode][toNode];
+	}
+
+	forEachNode(cb) {
+		for (let node in this.nodes) {
+			cb(node);
+		}
+	}
 }
