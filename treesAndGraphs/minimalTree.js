03e41f3ac0bad79f12f41ce516abc6424bd6855e
arsimr16 <arsimr16@g.holycross.edu>
2017-12-23 12:05:04 -0500

    write tests for minimalTree; TODO: create findDepth method for more efficient testing

--- treesAndGraphs/minimalTree.js
+++ treesAndGraphs/minimalTree.js
@@ -14,26 +14,29 @@ class BST {
 		const newNode = this.makeNode(value);
 		if (!this.root) {
 			this.root = newNode;
-		} else {
-			while (curr.value !== value) {
-				let curr = this.root;
-				if (value < curr.value) {
-					if (!this.left) {
-						this.left = newNode;
-						break;
-					}
-					curr = curr.left;
-				} else if (value > curr.value) {
-					if (!this.right) {
-						this.right = newNode;
-						break;
-					}
-					curr = curr.right;
-				}	
-			}
+			return;
+		}
+
+		let curr = this.root;
+		while (value !== curr.value) {
+			if (value < curr.value) {
+				if (!curr.left) {
+					curr.left = newNode;
+					break;
+				}
+				curr = curr.left;
+			} else if (value > curr.value) {
+				if (!curr.right) {
+					curr.right = newNode;
+					break;
+				}
+				curr = curr.right;
+			}	
 		}
 	}
 }
+
+
 
 // I: a sorted array (increasing) of unique integers
 // O: a BST
@@ -43,20 +46,48 @@ const minimalTree = (arr) => {
 	const result = new BST();
 	// inner recursive fn
 	const findNext = (arr) => {
-		if (arr.length > 0) {
-			const next = Math.floor(arr.length / 2);
-			result.insert(arr[next]);
-			const before = arr.slice(0, next);
-			const after = arr.slice(next);
-			findNext(before);
-			findNext(after);
-		}
+		const next = Math.floor((arr.length - 1) / 2);
+		result.insert(arr[next]);
+		const before = arr.slice(0, next);
+		const after = arr.slice(next + 1);
+		before.length ? findNext(before) : null;
+		after.length ? findNext(after) : null;
 	};
 
 	findNext(arr);
 	return result;
 };
-// time complexity:
-// space complexity:
+// time complexity: O(n)? I'm not sure about the time complexity of slice, 
+// but this might make it O(n^2) if each slice call is linear [(l) + (l - 1) + (l - 2)...]
+// space complexity: slice makes a copy of the array, so I think the time complexity is O(n^2)
 
 // tests
+const assertEquals = (actual, expected, testname) => {
+	if (actual === expected) {
+		console.log(`passed ${testname}`);
+	} else {
+		console.log(`FAILED ${testname}: expected "${expected}", but got "${actual}"`);
+	}
+};
+
+// TODO: write a function that returns the depth of a BST to make testing more efficient;
+const mt0 = minimalTree([2, 5, 6, 8, 9, 15, 17, 20, 23]);
+/*
+										9
+									/		\									
+								 /		 \
+							  5      17
+							 / \     / \
+						  2   6   15  20   
+									 \       \
+										8      23
+*/
+assertEquals(mt0.root.value, 9, 'minimalTree contains correct values in correct positions');
+assertEquals(mt0.root.left.value, 5, 'minimalTree contains correct values in correct positions');
+assertEquals(mt0.root.right.value, 17, 'minimalTree contains correct values in correct positions');
+assertEquals(mt0.root.left.left.value, 2, 'minimalTree contains correct values in correct positions');
+assertEquals(mt0.root.left.right.value, 6, 'minimalTree contains correct values in correct positions');
+assertEquals(mt0.root.right.left.value, 15, 'minimalTree contains correct values in correct positions');
+assertEquals(mt0.root.right.right.value, 20, 'minimalTree contains correct values in correct positions');
+assertEquals(mt0.root.left.right.right.value, 8, 'minimalTree contains correct values in correct positions');
+assertEquals(mt0.root.right.right.right.value, 23, 'minimalTree contains correct values in correct positions');
