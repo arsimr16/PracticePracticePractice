1e5ac81560b5d77e07440994608a17b8171be807
arsimr16 <arsimr16@g.holycross.edu>
2017-12-23 17:51:51 -0500

    problem set up for checkBalanced and validateBST

---
+++ treesAndGraphs/checkBalanced.js
@@ -0,0 +1,15 @@
+// implement a function to check whether a binary tree is balanced
+// a balanced tree is defined to be a tree such that the heights
+// of the two subtrees of any node never differ by more than 1
+
+// I: a binary search tree
+// O: a boolean - whether or not the BST is balanced (see def. above)
+// C: none
+// E: an empty tree is balanced
+const isBalanced = (BST) => {
+
+};
+// time complexity:
+// space complexity:
+
+// tests
---
+++ treesAndGraphs/validateBST.js
@@ -0,0 +1,13 @@
+// implement a function to check whether a binary tree is a binary search tree
+
+// I: a tree
+// O: a boolean - whether or not the input is a BST
+// C: none
+// E: none (if the tree contains duplicates, it cannot be a BST);
+const validateBST = (tree) => {
+	
+};
+// time complexity:
+// space complexity:
+
+// tests
