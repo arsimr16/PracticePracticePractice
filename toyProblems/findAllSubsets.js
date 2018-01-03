5f64a0474ed84bc0d5359e06e0a26a9eb376394f
arsimr16 <arsimr16@g.holycross.edu>
2018-01-03 15:37:48 -0500

    implement tests for findAllSubsets

--- toyProblems/findAllSubsets.js
+++ toyProblems/findAllSubsets.js
@@ -1,7 +1,7 @@
 //Find all subsets that sum up to k
 
-//subSets([1,2,1], 3) => [[1, 2], [2, 1]]
-//subSets([1,2,3,4,5,6], 10) => [[1, 2, 3, 4], [1, 3, 6], [1, 4, 5], [2, 3, 5], [4, 6]...]
+//subSets([1,2,1], 3) => [ [ 2, 1 ], [ 1, 2 ] ]
+//subSets([1,2,3,4,5,6], 10) => [ [ 4, 6 ], [ 2, 3, 5 ], [ 1, 4, 5 ], [ 1, 3, 6 ], [ 1, 2, 3, 4 ] ]
 
 // I: arr of integers, sum (k)
 // O: arr of arrays, each arr is individula ints that add up to k
@@ -14,21 +14,35 @@ const findAllSubsets = (integers, k) => {
   const findSubset = (i = 0, currSubset = [], sum = 0) => {
     // if k === 0, the curr subset is valid
     if (sum === k) {
-      results.push(currSubset);
+      return results.push(currSubset);
     }
     // if sum > k, the subset is invalid
     // if i >= integers.length, end recursive calls
     if (sum > k || i >= integers.length) {
       return;
     }
-    // include current int
-    findSubset(i + 1, currSubset.push(integers[i]), sum += integers[i]);
     // exclude current int
-    findSubset(i + 1, currSubset, sum);
+    findSubset(i + 1, currSubset.slice(), sum);
+    // include current int
+    currSubset.push(integers[i]);
+    findSubset(i + 1, currSubset.slice(), sum += integers[i]);
   };
+  findSubset();
   return results;
 };
 // time complexity: O(2^n) - exponential, each recursive call has two more possibilities
 // space complexity: O(2^n) - store all potential subsets
 
 // tests
+const assertDeepEquals = (actual, expected, testname) => {
+  if (JSON.stringify(actual) === JSON.stringify(expected)) {
+    console.log(`passed ${testname}`);
+  } else {
+    console.log(`FAILED ${testname}: expected ${expected}, but got ${actual}`);
+  }
+};
+
+assertDeepEquals(findAllSubsets([1, 2, 1], 3), [[2, 1], [1, 2]], 'should return all valid subsets');
+assertDeepEquals(findAllSubsets([1, 2, 3, 4, 5, 6], 10), [[4, 6], [2, 3, 5], [1, 4, 5], [1, 3, 6], [1, 2, 3, 4]], 'should return all valid subsets');
+assertDeepEquals(findAllSubsets([1, 2, 3], 10), [], 'should return empty array when there are no subsets');
+assertDeepEquals(findAllSubsets([2, 5, 9, 0], 3), [], 'should return empty array when there are no subsets');
