a1f20736ffcea2cf0736ee891f41a7fda1084473
arsimr16 <arsimr16@g.holycross.edu>
2018-01-09 11:26:41 -0500

    set up problem and define IOCE

---
+++ toyProblems/segment.js
@@ -0,0 +1,25 @@
+// given an array of integers and a length N, find all consecutive subarrays of length N in array.
+// find the minimum value in each subarray, then return the maximum of those values.
+
+// e.g.
+// input:
+//  	[3, 2, 5, 7, 0], 3
+
+// the subarrays are [[3, 2, 5], [2, 5, 7], [5, 7, 0]]
+// the mins of each are [2, 2, 5];
+// the max of the mins is 5
+
+// output:
+//  	5
+
+// I: an array of integers and a subarray length n
+// O: max of mins of each subarray (see example above)
+// C: none
+// E: assume n <= arr.length
+const seqment = (arr, n) => {
+
+};
+// time complexity:
+// space complexity:
+
+// tests:
