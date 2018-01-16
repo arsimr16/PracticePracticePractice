5e13bce4064e073f13cf45d584e688e5b3d96d4c
arsimr16 <arsimr16@g.holycross.edu>
2018-01-16 11:14:54 -0800

    implement working solution for leetcode 122

--- toyProblems/stockProfits.js
+++ toyProblems/stockProfits.js
@@ -1,9 +1,12 @@
 // LEETCODE #121 - Best time to buy and sell stock
 
 /*
-Say you have an array for which the ith element is the price of a given stock on day i.
+Say you have an array for which the ith element 
+is the price of a given stock on day i.
 
-If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+If you were only permitted to complete at most one transaction 
+(ie, buy one and sell one share of the stock), 
+design an algorithm to find the maximum profit.
 
 Example 1:
 Input: [7, 1, 5, 3, 6, 4]
@@ -30,4 +33,40 @@ var maxProfit = function(prices) {
     }
     return maxProfit;
 };
+
+
+
+
+
+// LEETCODE #122 - Best time to buy and sell stock II
+
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions 
+as you like (ie, buy one and sell one share of the stock multiple times). 
+However, you may not engage in multiple transactions at the same time 
+(ie, you must sell the stock before you buy again).
+*/
+
+// passes all test cases on leetcode
+var maxProfit = function(prices) {
+    let maxProfit = 0;
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            maxProfit += prices[i] - prices[i - 1];
+        }
+    }
+    return maxProfit;
+};
+
+
+
+
+
+// LEETCODE #123 - Best time to buy and sell stock III
+
+/*
+
+*/
 
