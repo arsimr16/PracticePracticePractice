649f31cd808032ffd07d3554085f3b942dd25048
arsimr16 <arsimr16@g.holycross.edu>
2018-01-17 21:58:17 -0800

    implement two sums II and pass all tests for leetcode 167

--- toyProblems/2Sum.js
+++ toyProblems/2Sum.js
@@ -31,4 +31,36 @@ var twoSum = function(nums, target) {
     return 'error, no match found';
 };
 
+// Leetcode #167 - Two Sum II - input array is sorted
 
+/*
+Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2
+*/
+
+// passes all tests on leetcode
+var twoSum = function(numbers, target) {
+		// store pointers to beginning and end of numbers array
+    let firstIndex = 0;
+    let secondIndex = numbers.length - 1;
+    // assuming there is always a valid solution and numbers is sorted, this will always work
+    while(true) {
+        let sum = numbers[firstIndex] + numbers[secondIndex];
+        if (sum === target) {
+        		// this problem states that the indices should not be 0-indexed
+            return [firstIndex + 1, secondIndex + 1];
+        // if sum is too small, try next largest number
+        } else if (sum < target) {
+            firstIndex++;
+        // if sum is too large, try next smallest number
+        } else if (sum > target) {
+            secondIndex--;
+        }
+    }
+};
