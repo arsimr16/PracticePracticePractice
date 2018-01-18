f4485fb5010ec3b97fed2cbde6c2b8a020fa0c3c
arsimr16 <arsimr16@g.holycross.edu>
2018-01-17 22:07:50 -0800

    implement two sums III and pass all tests for leetcode 170

--- toyProblems/2Sum.js
+++ toyProblems/2Sum.js
@@ -64,3 +64,39 @@ var twoSum = function(numbers, target) {
         }
     }
 };
+
+// Leetcode #170 - Two Sum III - data structure design
+
+/*
+Design and implement a TwoSum class. It should support the following operations: add and find.
+
+add - Add the number to an internal data structure.
+find - Find if there exists any pair of numbers which sum is equal to the value.
+
+For example,
+add(1); add(3); add(5);
+find(4) -> true
+find(7) -> false
+*/
+
+// passes all tests on leetcode
+var TwoSum = function() {
+    this.nums = [];
+};
+
+TwoSum.prototype.add = function(number) {
+    this.nums.push(number);
+};
+
+TwoSum.prototype.find = function(value) {
+    pairs = {};
+    for (let i = 0; i < this.nums.length; i++) {
+        const match = value - this.nums[i];
+        if (pairs[match]) {
+            return true;
+        } else {
+            pairs[this.nums[i]] = true;
+        }
+    }
+    return false;
+};
