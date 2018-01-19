eec0898b8310bb28e88d58d4b03a54f2f62e4846
arsimr16 <arsimr16@g.holycross.edu>
2018-01-18 18:52:44 -0800

    implement single number II and pass tests for leetcode 137

--- toyProblems/singleNumber.js
+++ toyProblems/singleNumber.js
@@ -21,3 +21,38 @@ var singleNumber = function(nums) {
 };
 // this implementation has O(n) time complexity and O(n) space complexity
 // I could avoid using extra space by sorting nums, but then the time would be O(n log n)
+
+
+
+
+
+
+// leetcode # 137 - single number III
+
+/*
+Given an array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+*/
+
+// passes all tests on leetcode
+var singleNumber = function(nums) {
+    const seen = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (seen[nums[i]]) {
+            if (seen[nums[i]] === 2) {
+                delete seen[nums[i]];
+            } else {
+                seen[nums[i]]++;
+            }
+        } else {
+            seen[nums[i]] = 1;
+        }
+    }
+    return parseInt(Object.keys(seen)[0]);
+};
+// this implementation has O(n) time complexity and O(n) space complexity
+// I could avoid using extra space by sorting nums, but then the time would be O(n log n)
+
+
