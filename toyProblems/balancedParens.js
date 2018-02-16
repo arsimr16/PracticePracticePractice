50ce81d6c35140c5418fc419041530627ccbb7dd
arsimr16 <arsimr16@g.holycross.edu>
2018-02-15 16:20:39 -0800

    added another balancing parens question

--- toyProblems/balancedParens.js
+++ toyProblems/balancedParens.js
@@ -59,3 +59,29 @@ const balancedParens = (input) => {
 };
 
 // this implementation passes all of the Hack Reactor tests
+
+
+// another parenthesis question
+/* A string of brackets is correctly matched if you can pair every opening bracket 
+up with a later closing bracket, and vice versa. For example, (()()) is correctly matched, 
+and (() and )( are not. Implement a function which takes a string of brackets and returns 
+the minimum number of brackets you'd have to add to the string to make it correctly matched.
+For example, (() could be correctly matched by adding a single closing bracket at the end, 
+so you'd return 1. )( can be correctly matched by adding an opening bracket at the start 
+and a closing bracket at the end, so you'd return 2. If your string is already correctly matched, 
+you can just return 0.
+*/
+
+const balancedParens2 = (str) {
+	let opening = 0;
+	let closing = 0;
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] === '(') {
+			opening++;
+		} else if (str[i] ===')') {
+			closing++;
+		}
+	}
+	return Math.abs(opening - closing);
+}
+// this implementation passed all tests that were given
