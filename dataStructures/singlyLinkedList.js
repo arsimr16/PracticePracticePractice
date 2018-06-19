fa705277e7177b0d12920b65347d24d86fc9cc43
arsimr16 <arsimr16@g.holycross.edu>
2018-06-19 14:47:08 -0700

    re-implement singlyLinkedList from scratch

--- dataStructures/singlyLinkedList.js
+++ dataStructures/singlyLinkedList.js
@@ -1,231 +1,141 @@
-// implement a sinlgyLinkedList with addToTail, removeHead, contains, and makeNode methods
+// implement a singlyLinkedList with addToTail, removeHead, contains, and makeNode methods
+// extra methods - add to head, remove, removeTail, forEach, map, insertAfter, insertBefore
 
 const LinkedList = () => {
 	this.head = null;
 	this.tail = null;
 };
 
-LinkedList.prototype.makeNode = (value) => {
-	const node = {};
-	node.value = value;
-	node.next = null;
-	return node;
-};
+LinkedList.prototype.makeNode = value => { value, 'next': null };
 
-LinkedList.prototype.addToTail = (value) => {
+LinkedList.prototype.addToTail = value => {
 	const newTail = this.makeNode(value);
-	// if there is no head, the new value will be the head
-	if (!this.head) {
+	if (this.head === null) {
 		this.head = newTail;
 	}
-	// if there is already a tail, the previous tail needs to point to the new tail
-	if (this.tail) {
+	if (this.tail !== null) {
 		this.tail.next = newTail;
 	}
-	// in all cases this.tail will be set to the new tail
 	this.tail = newTail;
 };
 
 LinkedList.prototype.removeHead = () => {
-	// store the current head
 	const prevHead = this.head;
-	// if there is no head, nothing can be removed, return null
-	if (!this.head) {
-		return null;
-	}
-	// if the head and tail are the same, there is one node,
-	// when it is removed, the head and tail will be null
+	if (prevHead === null) return;
 	if (this.head === this.tail) {
 		this.head = this.tail = null;
-	// else update head reference to next node
 	} else {
 		this.head = this.head.next;
 	}
-	// return the value of the removed head
 	return prevHead.value;
 };
 
-LinkedList.prototype.contains = (target) => {
-	let current = this.head;
-	while(current) {
-		if (current.value === target) {
+LinkedList.prototype.contains = target => {
+	let curr = this.head;
+	while(curr !== null) {
+		if (curr.value === target) {
 			return true;
 		}
-		current = current.next;
+		curr = curr.next;
 	}
 	return false;
 };
 
-
-// More methods - addToHead, insertAfter, insertBefore, remove, forEach, map
-
-
-LinkedList.prototype.addToHead = (value) => {
+LinkedList.prototype.addToHead = value => {
 	const newHead = this.makeNode(value);
-	// if there is no tail, the new head will also be the tail
-	if (!this.tail) {
+	if (this.tail === null) {
 		this.tail = newHead;
+	} else {
+		this.newHead.next = this.head;
 	}
-	// if there is already a head, set newHead.next to curr head
-	if (this.head) {
-		newHead.next = this.head;
-	}
-	// this.head will always be set to the new head
-	this.head = newHead;
+	this.head = this.newHead;
 };
 
-LinkedList.prototype.remove = (target) => {
-	// if the head === value, use removeHead method
-	if (this.head.value === target) {
-		return this.removeHead();
-	}
-	// iterate through linked list
+LinkedList.prototype.remove = target => {
 	const curr = this.head;
-	// while the current and next values exist
+	if (curr === null) {
+		return null;
+	} else if (curr.value === target) {
+		this.removeHead();
+	}
 	while(curr && curr.next) {
-		// if the next value === value, remove the node
 		if (curr.next.value === target) {
-			// if the node being removed has a node after it
-			if (curr.next.next) {
-				// update the pointer of the current node
+			if (curr.next.next !== null) {
 				curr.next = curr.next.next;
 			} else {
-				// otherwise, the current node should not point to anything
 				curr.next = null;
-				// the current node is now the new tail
 				this.tail = curr;
 			}
-			// return the value that was removed
 			return target;
 		}
-		// move to the next node in the LinkedList
-		curr = curr.next
+		curr = curr.next;
 	}
-	// return null if the item was not found
 	return null;
 };
 
-LinkedList.prototype.forEach = (cb) => {
-	let curr = this.head;
-	while(curr) {
-		// call the callback function on each item in the LinkedList
-		this.head.value = cb(this.head.value);
+LinkedList.prototype.removeTail = () => {
+	const oldTail = this.tail;
+	if (oldTail === null) return null;
+	if (oldTail === this.head) {
+		this.head = this.tail = null;
+		return oldTail.value;
+	}
+	const curr = this.head;
+	while(curr !== null) {
+		if (curr.next === oldTail) {
+			curr.next = null;
+			this.tail = curr;
+			return oldTail.value;
+		}
 		curr = curr.next;
 	}
-	// do not return anything
 };
 
-LinkedList.prototype.map = (cb) => {
-	// map should return a new List
-	const result = new LinkedList();
+LinkedList.prototype.forEach = cb => {
 	let curr = this.head;
-	while(curr) {
-		// add a new node to the tail of the result LinkedList 
-		// its value is the result of invoking the callback function on the current value
-		result.addToTail(cb(curr.value));
+	while(curr !== null) {
+		curr.value = cb(curr.value);
 		curr = curr.next;
 	}
+};
+
+LinkedList.prototype.map = cb => {
+	const result = new LinkedList();
+	this.forEach(node => result.addToTail(cb(node.value)));
 	return result;
 };
 
 LinkedList.prototype.insertAfter = (target, value) => {
 	const newNode = this.makeNode(value);
 	let curr = this.head;
-	while(curr) {
-		// if curr value === target, insert newNode
+	while(curr !== null) {
 		if (curr.value === target) {
-			// add pointer to following node
 			newNode.next = curr.next;
-			// update pointer of current node
 			curr.next = newNode;
-			// return value inserted
+			if (newNode.next === null) {
+				this.tail = newNode;
+			}
 			return value;
 		}
 		curr = curr.next;
 	}
-	// if target is not found, return null
-	return null
+	return null;
 };
 
 LinkedList.prototype.insertBefore = (target, value) => {
 	const newNode = this.makeNode(value);
-	// if head === target, invoke addToHead
 	if (this.head === target) {
 		return this.addToHead(value);
 	}
 	let prev = this.head;
 	let curr = this.head.next;
-	while(curr) {
-		// if the current value === target, insert between prev and curr
-		// I could invoke insertAfter(prev.value, value), but this would require iterating through the list again
-		// If I rewrite the logic of insertAfter, the code will not be as easy to read, but it will be faster
+	while(curr !== null) {
 		if (curr.value === target) {
-			// add pointer to curr node
 			newNode.next = curr;
-			// add pointer to prev node
-			prev.next = newNode;
-			// return value inserted
+			pre.next = newNode;
 			return value;
 		}
 		curr = curr.next;
 	}
-	// if target is not found, return null
 	return null;
 };
-
-
-// TODO add time and space complexities for all methods
-
-// TODO implement tests for all methods
-
-const assertEquals = (actual, expected, testName) => {
-	if (actual === expected) {
-		console.log(`passed "${testName}"`);
-	} else {
-		console.log(`FAILED "${testName}": expected: "${expected}", but got "${actual}"`);
-	}
-}
-
-// TESTS:
-
-// tests for addToTail()
-// 'should add to tail when list is empty' (check that start and end are same, value is correct)
-// 'should add to tail when list is not empty' (check that start and end are not same, end value is correct)
-
-// tests for makeNode()
-// 'this.value should equal passed in value'
-// 'this.next should equal null'
-
-// tests for removeHead()
-// 'should remove head if head exisits'
-// 'should also remove tail if list only had one item'
-// 'should not remove head if head does not exist'
-
-// tests for contains()
-// 'should return true if linked list contains value'
-// 'should return false if linked list does not contain value'
-
-// tests for addToHead()
-// 'should add to head when list is empty' (check that head and tail are same, value is correct)
-// 'should add to head when list is not empty' (check that head and tail are not same, head value is correct)
-
-// tests for insertAfter()
-// 'should insert new node after specified target'
-// 'should not insert value if target is not found'
-
-// tests for insertBefore()
-// 'should insert new node before specified target'
-// 'should not insert value if target is not found'
-
-// tests for remove()
-// 'should remove value if list contains value' 
-// 'should not remove anything is value is not found' (compare lengths of before and after?)
-
-// tests for forEach() 
-// 'should invoke the callback on each item in the LinkedList'
-// 'should not return anything'
-
-// tests for map()
-// 'should invoke callback on each item in the LinkedList'
-// 'should return new LinkedList with values being results of invoking cb on original values'
-// 'should not mutate original LinkedList'
